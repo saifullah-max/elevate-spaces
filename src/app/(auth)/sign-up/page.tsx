a2a3dd9ba633905a3c12b2cbc5387faa0a857b94
Saifullah Ahmed <saifullahahmed380@gmail.com>
2026-06-28 23:00:48 +0500

    hide apple/facebook icon for login/signup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -513,7 +513,7 @@ function SignUpForm() {
             </div>
           </div>
 
-          <div className="mt-6 grid grid-cols-3 gap-3">
+          <div className="mt-6 grid grid-cols-1 gap-3">
             <Button
               variant="outline"
               className="h-11 border-slate-300 hover:bg-slate-50 text-slate-700 hover:border-slate-400"
@@ -528,7 +528,7 @@ function SignUpForm() {
               </svg>
             </Button>
 
-            <Button
+            {/* <Button
               variant="outline"
               className="h-11 border-slate-300 hover:bg-slate-50 text-slate-700 hover:border-slate-400"
               type="button"
@@ -548,7 +548,7 @@ function SignUpForm() {
               <svg className="w-5 h-5" viewBox="0 0 16 16" fill="#1877F2">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
               </svg>
-            </Button>
+            </Button> */}
           </div>
         </div>
 
